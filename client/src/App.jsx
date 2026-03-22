574347fcc424a9b20747b90de8f00b570ca8db87
TheShohagMiah <themiahshohag@gmail.com>
2026-03-22 16:46:22 +0200

    i have set up folder structure for both of them-client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container-main">
+      <input type="text" placeholder="ENter your name" className="input" />
+      <button className="btn-primary">Button</button>
+    </div>
+  );
+};
+
+export default App;
